b3cb4bb46efde4d1d633dc8e5a5d359e59d1caca
Radim Halfar <radim.halfar@loopapps.com>
2017-09-05 13:13:26 +0200

    Fix incorrect view name

--- Source/SLKTextViewController.m
+++ Source/SLKTextViewController.m
@@ -2197,9 +2197,9 @@ CGFloat const SLKAutoCompletionViewDefaultHeight = 140.0;
     
     self.scrollViewProxy.translatesAutoresizingMaskIntoConstraints = false;
     self.autoCompletionView.translatesAutoresizingMaskIntoConstraints = false;
-    self.typingIndicatorView.translatesAutoresizingMaskIntoConstraints = false;
+    self.typingIndicatorProxyView.translatesAutoresizingMaskIntoConstraints = false;
     self.textInputbar.translatesAutoresizingMaskIntoConstraints = false;
-    
+
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[scrollView(0@750)][typingIndicatorView(0)]-0@999-[textInputbar(0)]|" options:0 metrics:nil views:views]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[autoCompletionView(0@750)][typingIndicatorView]" options:0 metrics:nil views:views]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[scrollView]|" options:0 metrics:nil views:views]];
